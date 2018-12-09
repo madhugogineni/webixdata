cfd7fd1935b0a24e770382013fb470021fd6454a
madhugogineni <mchaitanya92@gmail.com>
2018-12-09 22:20:06 +0530

    changes

--- webix-data.js
+++ webix-data.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+var mysqlcredentials = {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "webix"
+}
+var con = mysq.createConnection(mysqlcredentials);
+con.connect(function(error) {
+    if(error) throw error;
+    console.log("mysql connected");
+
+})
 app.get("/",function(req,res) {
     console.log("welcome");
-    res.send("working!")
+    con.query("select * from data",function(error,result) {
+        console.log(result);
+    });
 });
 app.listen("3000",function() {
     console.log("connnected to port 4000");
