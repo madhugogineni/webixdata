7e4bb01432a479e8f53003596bcfcbcc6d52fe9a
madhugogineni <mchaitanya92@gmail.com>
2018-12-10 17:46:13 +0530

    changes

--- webix-data.js
+++ webix-data.js
@@ -10,26 +10,52 @@ var mysqlcredentials = {
     database: "webix"
 }
 var con = mysql.createConnection(mysqlcredentials);
-con.connect(function(error) {
-    if(error) throw error;
+con.connect(function (error) {
+    if (error) throw error;
     console.log("mysql connected");
 
 })
-app.get("/",function(req,res) {
+app.get("/", function (req, res) {
     console.log("welcome");
-    con.query("select * from data",function(error,result) {
+    con.query("select * from data", function (error, result) {
         console.log(result);
         res.json(result);
     });
 });
-app.get("/filter",function(req,res) {
+app.get("/filter", function (req, res) {
     console.log("welcome");
-    con.query("select * from data",function(error,result) {
-        var id = req.query.direction;
-        console.log(id);
+    var direction = req.query.direction;
+    var date = req.query.date;
+    var price = req.query.price;
+    var save = req.query.save;
+    var places = req.query.places;
+    var status = req.query.status;
+    var key = null,value = null;
+    if(direction == undefined) {
+        key = "direction";
+        value = "%"+direction+"%";
+    }else if(date == undefined) {
+        key = "date";
+        value = "%"+date+"%";
+    }else if(price == undefined) {
+        key = "price";
+        value = "%"+price+"%";
+    }else if(save == undefined) {
+        key = "save";
+        value = "%"+save+"%";
+    }else if(places == undefined) {
+        key = "places";
+        value = "%"+places+"%";
+    }else if(status == undefined) {
+        key = "status";
+        value = "%"+status+"%";
+    }
+
+    con.query("select * from data where "+key+" like "+value, function (error, result) {
+        //date,price,save,palces,status
         res.json(result);
     });
 });
-app.listen("3000",function() {
+app.listen("3000", function () {
     console.log("connnected to port 4000");
 })
